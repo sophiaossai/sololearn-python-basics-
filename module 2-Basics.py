0b30b0169927b69fc69b75736dbf251cde11dbe3
sophiaossai <ossaisophia07@gmail.com>
2026-05-13 19:36:00 +0100

    feat: completed SoloLearn Intro to Python Module 2 - debugging, variables, data types

---
+++ module 2-Basics.py
@@ -0,0 +1,25 @@
+SoloLearn Intro to Python - Module  
+Topics: Debugging, Variables, Data Types, Input/Output, Best Practices
+
+ 1. Three steps in coding: Writing, Executing, Fixing errors
+ Example: print("Hello") -> run -> fix typo if error
+2. Variables and data types
+name = "Alice"- string
+age = 20 - integer
+gpa = 3.70 -float
+
+print(name, age, gpa)
+
+3. Input and output
+user_input = input(Sophia: ")
+print("Hello,", user_input)
+
+ 4. Comments and best practices 
+Use comments to explain code-# declaring a varaiable
+Use lowercase with underscores for variables: user_name
+Python is case sensitive: Name != name
+
+ 5. Debugging example
+num = 5
+print(numm): This would cause NameError
+print(num)-Bug fixed 
