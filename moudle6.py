7ed68bc7942c5788b94ded2d7d1df2e3de4bc8d7
sophiaossai <ossaisophia07@gmail.com>
2026-05-20 22:01:18 +0100

    feat: module 6 - dictionaries

--- moudle6.py
+++ moudle6.py
@@ -1,1 +1,10 @@
 moudle6-Dictionaries 
+ Module 6 - Dictionaries
+student = {"name": "Sophia", "age": 19}
+
+print(student["name"])
+student["grade"] = "A"
+print(student)
+
+for key, value in student.items():
+    print(f"{key}: {value}")
